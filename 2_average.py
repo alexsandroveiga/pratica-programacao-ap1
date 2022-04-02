966145f151c5827910d4598de0fc953932b27a8d
Alexsandro <sandro.alex_@hotmail.com>
2022-04-02 11:19:27 -0300

    feat: add average app

---
+++ 2_average.py
@@ -0,0 +1,7 @@
+firstGrade = int(input('Digite a primeira nota: '))
+secondGrade = int(input('Digite a segunda nota: '))
+thirdGrade = int(input('Digite a terceira nota: '))
+fourthGrade = int(input('Digite a quarta nota: '))
+average = (firstGrade + secondGrade + thirdGrade + fourthGrade) / 4
+print('Sua média é:', average)
+
