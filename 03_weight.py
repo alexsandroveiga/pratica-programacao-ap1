059de6f35833cca6053549818f3e388d38b15cb7
Alexsandro <sandro.alex_@hotmail.com>
2022-04-03 14:04:25 -0300

    feat: update weight app

--- 03_weight.py
+++ 03_weight.py
@@ -4,18 +4,11 @@ while True:
     height = float(height.replace(',', '.'))
     break
   except ValueError:
-    print('Por favor, digite sua idade corretamente...')  
+    print('Por favor, digite sua altura corretamente...')  
     continue
 
-sex = input('Digite seu sexo (M ou F): ').upper()
-
-while sex not in ('M','F','MASCULINO', 'FEMININO'):
-  sex = input('Digite seu sexo corretamente: ').upper()
-
-if sex == 'M' or sex == 'MASCULINO':
-  weight = (72.7 * height) - 58
-  print('Seu peso ideal é:', '{:.2f}kg'.format(weight))
-
-if sex == 'F' or sex == 'FEMINIMO':
-  weight = (62.1 * height) - 44.7
-  print('Seu peso ideal é:', '{:.2f}kg'.format(weight))
+mensIdealWeight = (72.7 * height) - 58
+womensIdealWeight = (62.1 * height) - 44.7
+print('Peso ideal')
+print('Para homens:', '{:.2f}kg'.format(mensIdealWeight))
+print('Para mulheres:', '{:.2f}kg'.format(womensIdealWeight))
